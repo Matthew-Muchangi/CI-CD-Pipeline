744b022d99c98ba09b25cb7c627e8c065be62710
Matthew-Muchangi <matthewmuchangi@gmail.com>
2025-09-30 18:47:14 +0300

    Updated MongoDB Atlas connection string

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://matthewmuchangi:much%40ngi12@cluster0.mokxjvb.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://matthewmuchangi:much%40ngi12@cluster0.mokxjvb.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://matthewmuchangi:much%40ngi12@cluster0.mokxjvb.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster0',
 }
+
 module.exports = config;
+
