f6d96e18b1dda8a780b3d1db5be86f178c44a230
Matthew-Muchangi <matthewmuchangi@gmail.com>
2025-09-30 21:43:01 +0300

    Added MILESTONE 2 to landing page

--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,50 @@ const express = require('express');
 const router = express.Router();
 const uuid = require('uuid');
 let upload = require('./upload');
-const url = require('url')
+const url = require('url');
 let Image = require('../models/images');
 
-
-var db = []
-
-router.get('/', (req,res)=>{
-    
-    Image.find({}, function(err, images){
-        // console.log(images)
-        if (err) console.log(err);
-        res.render('index',{images:images, msg: req.query.msg })
-    })
-})
-
-router.post('/upload', (req, res)=>{
-    upload(req,res, (err)=>{
-        if (err){
-            res.redirect(`/?msg=${err}`);
-        }else{
-            console.log(req.file);
-            // res.send("test");
-            if (req.file == undefined){
-                res.redirect('/?msg=Error: No file selcted!');
-            }else{
-                // const imageObj = {
-                //     id: uuid.v4(),
-                //     name: req.file.filename,
-                //     path: 'images/' + req.file.filename
-                // }
-                // db.push(imageObj);
-                // console.log(db);
-
-                // create new image
-                let newImage = new Image({
-                    name: req.file.filename,
-                    size: req.file.size,
-                    path: 'images/' + req.file.filename
-                })
-
-                // save the uploaded image to the database
-                newImage.save()
-
-                
-                res.redirect('/?msg=File uploaded successfully');
-            }
+// Home route
+router.get('/', (req, res) => {
+    Image.find({}, function(err, images) {
+        if (err) {
+            console.error(err);
+            // Send empty array if DB fails (so the page still loads)
+            return res.render('index', { images: [], msg: "Error fetching images" });
         }
-    })
-})
+        // Always send an array, even if no images are found
+        res.render('index', { images: images || [], msg: req.query.msg });
+    });
+});
+
+// Upload route
+router.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.redirect(`/?msg=${err}`);
+        }
+
+        if (!req.file) {
+            return res.redirect('/?msg=Error: No file selected!');
+        }
+
+        console.log("Uploaded file:", req.file);
+
+        // Create new image document
+        let newImage = new Image({
+            name: req.file.filename,
+            size: req.file.size,
+            path: 'images/' + req.file.filename
+        });
+
+        // Save the uploaded image to the database
+        newImage.save()
+            .then(() => res.redirect('/?msg=File uploaded successfully'))
+            .catch(saveErr => {
+                console.error(saveErr);
+                res.redirect('/?msg=Error saving file to database');
+            });
+    });
+});
 
 module.exports = router;
